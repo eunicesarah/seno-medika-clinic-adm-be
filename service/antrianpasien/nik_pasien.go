9d07e860f6fe8bf8d1603423cc3534859dad0b65
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-05 18:35:20 +0700

    feat(antrian): convert nik to pasien id

---
+++ service/antrianpasien/nik_pasien.go
@@ -0,0 +1,15 @@
+package antrianpasien
+
+import (
+	"seno-medika.com/config/db"
+)
+
+func ConvertNIKtoPasienID(nik string) (int, error) {
+	var pasienID int
+	err := db.DB.QueryRow("SELECT pasien_id FROM pasien WHERE nik = $1", nik).Scan(&pasienID)
+	if err != nil {
+		return 0, err
+	}
+
+	return pasienID, nil
+}
