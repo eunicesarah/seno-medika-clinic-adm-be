9dc9990a56cf52601420dad9069a4e439a6b96cd
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-05 18:34:23 +0700

    feat(antrian): model antrian online

--- controller/antrian/antrian.go
+++ controller/antrian/antrian.go
@@ -8,6 +8,7 @@ import (
 	"seno-medika.com/config/db"
 	"seno-medika.com/model/common"
 	"seno-medika.com/model/antrian"
+	"seno-medika.com/service/antrianpasien"
 )
 
 // TODO: get pasien from list
@@ -29,6 +30,68 @@ func AddAntrianOffline(c *gin.Context) {
 	// antrian.PasienID = 
 
 	_, err := db.DB.Exec(
+		`INSERT INTO antrian (
+			pasien_id,
+			nomor_antrian,
+			status,
+			poli,
+			instalasi,
+			created_at
+		) VALUES ($1, $2, $3, $4, $5, $6)`,
+		antrian.PasienID,
+		antrian.NomorAntrian,
+		antrian.Status,
+		antrian.Poli,
+		antrian.Instalasi,
+		antrian.CreatedAt)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, common.Response{
+			Message:    err.Error(),
+			Status:     "Internal Server Error",
+			StatusCode: http.StatusInternalServerError,
+			Data:       nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, common.Response{
+		Message:    "Antrian berhasil ditambahkan",
+		Status:     "Created",
+		StatusCode: http.StatusOK,
+		Data:       nil,
+	})
+}
+
+func AddAntrianOnline(c *gin.Context) {
+	var antrianOnline antrian.AntrianOnline
+	var antrian antrian.Antrian
+	
+	if err := c.ShouldBind(&antrianOnline); err != nil {
+		c.JSON(http.StatusBadRequest, common.Response{
+			Message:    err.Error(),
+			Status:     "Bad Request",
+			StatusCode: http.StatusBadRequest,
+			Data:       nil,
+		})
+		return
+	}
+
+	pasienID, err := antrianpasien.ConvertNIKtoPasienID(antrianOnline.NIK)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, common.Response{
+			Message:    err.Error(),
+			Status:     "Internal Server Error",
+			StatusCode: http.StatusInternalServerError,
+			Data:       nil,
+		})
+		return
+	}
+
+	antrian.PasienID = pasienID
+	antrian.NomorAntrian = antrianOnline.NomorAntrian
+
+	_, err = db.DB.Exec(
 		`INSERT INTO antrian (
 			pasien_id,
 			nomor_antrian,
