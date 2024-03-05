925b634bce03cb0ebab1c007029ff9b05b72eeb3
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-05 15:55:56 +0700

    feat(pendaftaranpasien): controller update pasien

--- controller/pendaftaranpasien/pendaftaran_pasien.go
+++ controller/pendaftaranpasien/pendaftaran_pasien.go
@@ -1,6 +1,5 @@
 package pendaftaranpasien
 
-
 import (
 	"net/http"
 	"time"
@@ -119,7 +118,7 @@ func AddPasien(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, common.Response{
-		Message:    "Successfully insert user",
+		Message:    "Successfully insert pasien",
 		Status:     "ok",
 		StatusCode: http.StatusOK,
 		Data:       nil,
@@ -127,4 +126,108 @@ func AddPasien(c *gin.Context) {
 
 }
 
-// TODO: get user that updated the data
+// TODO: 
+// - get user that updated the data
+// - updateBy and target
+func UpdatePasien(c *gin.Context) {
+	// updateBy := c.Query("update_by")
+	// target := c.Query("target")
+	pasienTarget := c.Query("pasien_target")
+	var pasien person.Pasien
+
+	if err := c.ShouldBind(&pasien); err != nil {
+		c.JSON(http.StatusBadRequest, common.Response{
+			Message:    err.Error(),
+			Status:     "Bad Request",
+			StatusCode: http.StatusBadRequest,
+			Data:       nil,
+		})
+		return
+	}
+
+	pasien.UpdatedAt = time.Now().Local().String()
+	// pasien.UpdatedBy
+
+	_, err := db.DB.Exec(
+		`UPDATE pasien SET
+			no_erm = $1,
+			no_rm_lama = $2,
+			no_dok_rm = $3,
+			penjamin = $4,
+			no_penjamin = $5,
+			nik = $6,
+			no_kk = $7,
+			nama = $8,
+			tempat_lahir = $9,
+			tanggal_lahir = $10,
+			no_ihs = $11,
+			jenis_kelamin = $12,
+			golongan_darah = $13,
+			no_telpon = $14,
+			email = $15,
+			provinsi = $16,
+			kabupaten_kota = $17,
+			kecamatan = $18,
+			kelurahan = $19,
+			alamat = $20,
+			nama_kontak_darurat = $21,
+			nomor_kontak_darurat = $22,
+			pekerjaan = $23,
+			agama = $24,
+			warga_negara = $25,
+			pendidikan = $26,
+			status_perkawinan = $27,
+			updated_at = $28,
+			updated_by = $29
+		WHERE pasien_uuid = $30
+		`,
+		pasien.NoERM,
+		pasien.NoRMLama,
+		pasien.NoDokRM,
+		pasien.Penjamin,
+		pasien.NoPenjamin,
+		pasien.NIK,
+		pasien.NoKK,
+		pasien.Nama,
+		pasien.TempatLahir,
+		pasien.TanggalLahir,
+		pasien.NoIHS,
+		pasien.JenisKelamin,
+		pasien.GolonganDarah,
+		pasien.NoTelpon,
+		pasien.Email,
+		pasien.Provinsi,
+		pasien.KabupatenKota,
+		pasien.Kecamatan,
+		pasien.Kelurahan,
+		pasien.Alamat,
+		pasien.NamaKontakDarurat,
+		pasien.NomorKontakDarurat,
+		pasien.Pekerjaan,
+		pasien.Agama,
+		pasien.WargaNegara,
+		pasien.Pendidikan,
+		pasien.StatusPerkawinan,
+		pasien.UpdatedAt,
+		pasien.UpdatedBy,
+		pasienTarget)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, common.Response{
+			Message:    err.Error(),
+			Status:     "Internal Server Error",
+			StatusCode: http.StatusInternalServerError,
+			Data:       nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, common.Response{
+		Message:    "Successfully update pasien",
+		Status:     "ok",
+		StatusCode: http.StatusOK,
+		Data:       nil,
+	})
+}
+
+
