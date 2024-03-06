3526caf9c088714d4c48be8ed1ba32083a13ec88
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-03-06 17:40:53 +0700

    add: find_rm_test.go

--- service/superadmin/find_rm_test.go
+++ service/superadmin/find_rm_test.go
@@ -4,8 +4,113 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"seno-medika.com/config/db"
+	"seno-medika.com/model/person"
 	"testing"
 )
+
+func TestFindById_Fail(t *testing.T) {
+	val, err := FindById(1)
+	require.Error(t, err)
+	require.Equal(t, person.UserWithoutPassword{}, val)
+}
+
+func TestFindById_Success(t *testing.T) {
+	_db := db.DB
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 2")
+		db.DB = _db
+	}()
+
+	_db.Exec("INSERT INTO users (user_id, user_uuid, nama, email, role, password) VALUES ($2, $1, 'test', 'test', 'test','akjfkjdfkskdf')", uuid.New(), 2)
+
+	val, err := FindById(2)
+	t.Log(val)
+	require.NoError(t, err)
+}
+
+func TestFindByUuid_Fail(t *testing.T) {
+	val, err := FindByUuid(uuid.New().String())
+	require.Error(t, err)
+	require.Equal(t, person.UserWithoutPassword{}, val)
+}
+
+func TestFindByUuid_Success(t *testing.T) {
+	_db := db.DB
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 3")
+		db.DB = _db
+	}()
+
+	uid := uuid.New()
+	_db.Exec("INSERT INTO users (user_id, user_uuid, nama, email, role, password) VALUES (3, $1, 'test', 'test', 'test','akjfkjdfkskdf')", uid)
+
+	val, err := FindByUuid(uid.String())
+	require.NoError(t, err)
+	require.Equal(t, uid, val.UserUUID)
+}
+
+func TestFindByName_Fail(t *testing.T) {
+	val, err := FindByName("test")
+	require.NoError(t, err)
+	require.Equal(t, []person.UserWithoutPassword(nil), val)
+}
+
+func TestFindByName_Success(t *testing.T) {
+	_db := db.DB
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 4")
+		db.DB = _db
+	}()
+
+	uid := uuid.New()
+	_db.Exec("INSERT INTO users (user_id, user_uuid, nama, email, role, password) VALUES (4, $1, 'test', 'test', 'test','akjfkjdfkskdf')", uid)
+
+	val, err := FindByName("test")
+	require.NoError(t, err)
+	require.Equal(t, "test", val[0].Nama)
+}
+
+func TestFindByEmail_Fail(t *testing.T) {
+	val, err := FindByEmail("test")
+	require.NoError(t, err)
+	require.Equal(t, []person.UserWithoutPassword(nil), val)
+}
+
+func TestFindByEmail_Success(t *testing.T) {
+	_db := db.DB
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 5")
+		db.DB = _db
+	}()
+
+	uid := uuid.New()
+	_db.Exec("INSERT INTO users (user_id, user_uuid, nama, email, role, password) VALUES (5, $1, 'test', 'test', 'test','akjfkjdfkskdf')", uid)
+
+	val, err := FindByEmail("test")
+	require.NoError(t, err)
+	require.Equal(t, "test", val[0].Email)
+}
+
+func TestFindByRole_Fail(t *testing.T) {
+	val, err := FindByRole("test")
+	require.NoError(t, err)
+	require.Equal(t, []person.UserWithoutPassword(nil), val)
+}
+
+func TestFindByRole_Success(t *testing.T) {
+	_db := db.DB
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 6")
+		db.DB = _db
+	}()
+
+	uid := uuid.New()
+	_db.Exec("INSERT INTO users (user_id, user_uuid, nama, email, role, password) VALUES (6, $1, 'test', 'test', 'test','akjfkjdfkskdf')", uid)
+
+	val, err := FindByRole("test")
+	require.NoError(t, err)
+	require.Equal(t, "test", val[0].Role)
+}
 
 func TestFindAll_Success(t *testing.T) {
 	_db := db.DB
@@ -33,20 +138,7 @@ func TestFindAll_Fail(t *testing.T) {
 
 	_db.Close()
 
-	_, err := FindAll()
+	val, err := FindAll()
 	require.Error(t, err)
+	require.Equal(t, []person.UserWithoutPassword(nil), val)
 }
-
-//func TestFindById_Success(t *testing.T) {
-//	_db := db.DB
-//	defer func() {
-//		_db.Exec("DELETE FROM users WHERE user_id = 2")
-//		db.DB = _db
-//	}()
-//
-//	_db.Exec("INSERT INTO users (user_id, user_uuid, nama, email, role, password) VALUES ($2, $1, 'test', 'test', 'test','akjfkjdfkskdf')", uuid.New(), 2)
-//
-//	val, err := FindById(2)
-//	t.Log(val)
-//	require.NoError(t, err)
-//}
