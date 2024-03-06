124d53febd6b654c738e3b42247589e09bdc7852
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-03-06 18:48:54 +0700

    fixed issues : testing superadmin delete_rm_test.go

--- service/superadmin/delete_rm_test.go
+++ service/superadmin/delete_rm_test.go
@@ -59,3 +59,84 @@ func TestDeleteUserByUuid_Fail(t *testing.T) {
 	err := DeleteUserByUuid(uuid.New().String())
 	require.Error(t, err)
 }
+
+func TestDeleteUserByEmail_Success(t *testing.T) {
+	_db := db.DB
+
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 1")
+		db.DB = _db
+	}()
+
+	_db.Query("INSERT INTO users (user_id, user_uuid, nama, email, password, role) VALUES (1, $1, 'test', 'test', '', '')", uuid.New())
+
+	err := DeleteUserByEmail("test")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func TestDeleteUserByEmail_Fail(t *testing.T) {
+	_db := db.DB
+
+	defer func() {
+		db.DB = _db
+	}()
+
+	err := DeleteUserByEmail("test")
+	require.Error(t, err)
+}
+
+func TestDeleteUserByName_Success(t *testing.T) {
+	_db := db.DB
+
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 1")
+		db.DB = _db
+	}()
+
+	_db.Query("INSERT INTO users (user_id, user_uuid, nama, email, password, role) VALUES (1, $1, 'test', 'test', '', '')", uuid.New())
+
+	err := DeleteUserByName("test")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func TestDeleteUserByName_Fail(t *testing.T) {
+	_db := db.DB
+
+	defer func() {
+		db.DB = _db
+	}()
+
+	err := DeleteUserByName("test")
+	require.Error(t, err)
+}
+
+func TestDeleteUserByRole_Success(t *testing.T) {
+	_db := db.DB
+
+	defer func() {
+		_db.Exec("DELETE FROM users WHERE user_id = 1")
+		db.DB = _db
+	}()
+
+	_db.Query("INSERT INTO users (user_id, user_uuid, nama, email, password, role) VALUES (1, $1, 'test', 'test', '', '')", uuid.New())
+
+	err := DeleteUserByRole("test")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func TestDeleteUserByRole_Fail(t *testing.T) {
+	_db := db.DB
+
+	defer func() {
+		db.DB = _db
+	}()
+
+	err := DeleteUserByRole("test")
+	require.Error(t, err)
+}
