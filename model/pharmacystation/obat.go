91b762d42b756ca174e7a3485c39cacff5ddce9d
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-15 13:19:14 +0700

    feat(model):  obat

---
+++ model/pharmacystation/obat.go
@@ -0,0 +1,15 @@
+package pharmacystation
+
+type Obat struct {
+	ObatID        int    `json:"obat_id"`
+	NamaObat      string `json:"nama_obat"`
+	JenisAsuransi string `json:"jenis_asuransi"`
+	Harga         int    `json:"harga"`
+}
+
+type ListObat struct {
+	ObatID  int    `json:"obat_id"`
+	ResepID int    `json:"resep_id"`
+	Jumlah  int    `json:"jumlah"`
+	Dosis   string `json:"dosis"`
+}
