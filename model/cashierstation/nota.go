e407d4abeeaaf934066cfd4feafe7eae2f50206d
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-04-28 20:41:22 +0700

    feat(model): fix obat, resep, tindakan, pemeriksaan

--- model/cashierstation/nota.go
+++ model/cashierstation/nota.go
@@ -3,17 +3,18 @@ package cashierstation
 import "seno-medika.com/model/pharmacystation"
 
 type Nota struct {
-	NotaID     int `json:"nota_id"`
-	PasienID   int `json:"pasien_id"`
-	DokterID   int `json:"dokter_id"`
-	ResepID    int `json:"resep_id"`
-	ListTindakanID int `json:"list_tindakan_id"`
-	TotalBiaya int `json:"total_biaya"`
+	NotaID           int    `json:"nota_id"`
+	PasienID         int    `json:"pasien_id"`
+	DokterID         int    `json:"dokter_id"`
+	ResepID          int    `json:"resep_id"`
+	ListTindakanID   int    `json:"list_tindakan_id"`
+	TotalBiaya       int    `json:"total_biaya"`
 	MetodePembayaran string `json:"metode_pembayaran"`
 }
 
 type ListTindakan struct {
 	ListTindakanID int `json:"list_tindakan_id"`
+	TindakanID     int `json:"tindakan_id"`
 }
 
 type Tindakan struct {
@@ -24,16 +25,15 @@ type Tindakan struct {
 }
 
 type Penanganan struct {
-	TindakanID     int `json:"tindakan_id"`
 	ListTindakanID int `json:"list_tindakan_id"`
 }
 type DetailNota struct {
-	pharmacystation.Obat `json:"-"`
+	pharmacystation.Obat     `json:"-"`
 	pharmacystation.ListObat `json:"-"`
-	NamaObat             string `json:"nama_obat"`
-	Harga                int    `json:"harga"`
-	Jumlah               int    `json:"jumlah"`
-	Dosis                string `json:"dosis"`
+	NamaObat                 string `json:"nama_obat"`
+	Harga                    int    `json:"harga"`
+	Jumlah                   int    `json:"jumlah"`
+	Dosis                    string `json:"dosis"`
 }
 
 type DetailTindakan struct {
