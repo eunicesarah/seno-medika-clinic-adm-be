86019064abc408943e0834a3ae96c50c5093cb2d
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-04-30 18:35:35 +0700

    feat: update find pasien by name

--- query/pasien/find_ps.go
+++ query/pasien/find_ps.go
@@ -194,3 +194,58 @@ func FindPasienByNIK(nik string) (person.Pasien, error) {
 
 	return pasienVar, nil
 }
+
+func FindPasienByName(name string) ([]person.Pasien, error) {
+	var pasienVar []person.Pasien
+
+	val, err := db.DB.Query("SELECT * FROM pasien WHERE nama LIKE $1", name)
+	if err != nil {
+		return nil, err
+	}
+
+	for val.Next() {
+		var eachPasien person.Pasien
+		err := val.Scan(
+			&eachPasien.PasienID,
+			&eachPasien.NoERM,
+			&eachPasien.PasienUUID,
+			&eachPasien.NoRMLama,
+			&eachPasien.NoDokRM,
+			&eachPasien.Penjamin,
+			&eachPasien.NoPenjamin,
+			&eachPasien.NIK,
+			&eachPasien.NoKK,
+			&eachPasien.Nama,
+			&eachPasien.TempatLahir,
+			&eachPasien.TanggalLahir,
+			&eachPasien.NoIHS,
+			&eachPasien.JenisKelamin,
+			&eachPasien.GolonganDarah,
+			&eachPasien.NoTelpon,
+			&eachPasien.Email,
+			&eachPasien.Provinsi,
+			&eachPasien.KabupatenKota,
+			&eachPasien.Kecamatan,
+			&eachPasien.Kelurahan,
+			&eachPasien.Alamat,
+			&eachPasien.NamaKontakDarurat,
+			&eachPasien.NomorKontakDarurat,
+			&eachPasien.Pekerjaan,
+			&eachPasien.Agama,
+			&eachPasien.WargaNegara,
+			&eachPasien.Pendidikan,
+			&eachPasien.StatusPerkawinan,
+			&eachPasien.CreatedAt,
+			&eachPasien.CreatedBy,
+			&eachPasien.UpdatedAt,
+			&eachPasien.UpdatedBy,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		pasienVar = append(pasienVar, eachPasien)
+	}
+
+	return pasienVar, nil
+}
