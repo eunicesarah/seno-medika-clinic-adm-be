933732323dbfe921d731ed44adc6cbd8c68e89d7
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-05-06 14:15:02 +0700

    fix: patch cors

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"seno-medika.com/router"
-	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	r := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:3000"}
-	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}
 	r.Use(cors.New(config))
 
 	router.MainRouter(r)
